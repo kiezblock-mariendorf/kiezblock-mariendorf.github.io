0789fe129207773202abcce34783ffb7f3c9028e
elFeliz <felix.kunze@t-online.de>
2024-05-07 21:09:11 +0200

    fix: use new url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config : Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://kiez-mariendorf.de',
+  url: 'https://kiezblock-mariendorf.de',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
